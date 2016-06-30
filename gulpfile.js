123a14674318b99672d34888f45b24720df2a69a
Justin <Justin.D.Johnson@hotmail.com>
2016-06-30 16:33:50 -0400

    build setup, separate from src

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+var gulp = require('gulp'),
+	del = require('del'),
+    path = require('path'),
+    Builder = require('systemjs-builder'),
+    ts = require('gulp-typescript'),
+    sourcemaps  = require('gulp-sourcemaps');
+
+var tsProject = ts.createProject('tsconfig.json');
+
+var appDev = 'app'; // where your ts files are, whatever the folder structure in this folder, it will be recreated in the below 'dist/app' folder
+var appProd = 'dist/app';
+
+/** first transpile ts files */
+gulp.task('ts', () => {
+    return gulp.src(appDev + '/**/*.ts')
+        .pipe(sourcemaps.init({
+            loadMaps: true
+        }))
+        .pipe(ts(tsProject))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(appProd));
+});
+
+/** then bundle */
+gulp.task('bundle', ['ts'], function() {
+    // optional constructor options
+    // sets the baseURL and loads the configuration file
+    var builder = new Builder('', 'systemjs.config.js');
+
+    /*
+       the parameters of the below buildStatic() method are:
+           - your transcompiled application boot file (the one wich would contain the bootstrap(MyApp, [PROVIDERS]) function - in my case 'dist/app/boot.js'
+           - the output (file into which it would output the bundled code)
+           - options {}
+    */
+    return builder
+        .buildStatic(appProd + '/main.js', appProd + '/bundle.js', { minify: true, sourceMaps: true})
+        .then(function() {
+            console.log('Build complete');
+        })
+        .catch(function(err) {
+            console.log('Build error');
+            console.log(err);
+        });
+});
+
+/** this builds for prod, with bundling */
+gulp.task('build', ['ts', 'bundle']);
+
+/** removes dist directory */
+gulp.task('clean', (cb) => {
+    return del(["dist"], cb);
+});
+
+/** copies necessary resources into dist directory */
+gulp.task('resources', ['clean'], () => {
+    return gulp.src(["!**/*.ts", "!node_modules/", "!node_modules/**", "!dist/", "!dist/**", "**/*"])
+        .pipe(gulp.dest("dist"));
+});
+
+
+gulp.task('libs', ['resources'], () => {
+	return gulp.src([
+            'es6-shim/es6-shim.min.js',
+            'reflect-metadata/Reflect.js',
+            'zone.js/dist/**'
+        ], {cwd: "node_modules/**"}) /* Glob required here. */
+        .pipe(gulp.dest("dist/lib"));
+});
+
+gulp.task('ready', ['clean', 'resources', 'libs', 'build']);
--- systemjs.config.js
+++ systemjs.config.js
@@ -2,16 +2,18 @@
 
   // map tells the System loader where to look for things
   var map = {
-    'app':                        'app', // 'dist',
+    'app':                        'dist/app',
     'rxjs':                       'node_modules/rxjs',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
     '@angular':                   'node_modules/@angular',
-	'moment': 					  'node_modules/moment/moment.js'
+	'moment':					  'node_modules/moment/moment.js',
+	'ng2-bootstrap':			  'node_modules/ng2-bootstrap/'
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
-    'app':                        { main: 'main.js',  defaultExtension: 'js' },
+    'app':                        { main: 'main.js', defaultExtension: 'js' },
+    'ng2-bootstrap':              { main: 'bundles/ng2-bootstrap.min.js', defaultExtension:'js' },
     'rxjs':                       { defaultExtension: 'js' },
     'angular2-in-memory-web-api': { defaultExtension: 'js' },
   };
@@ -23,7 +25,6 @@
     '@angular/http',
     '@angular/platform-browser',
     '@angular/platform-browser-dynamic',
-    '@angular/router',
     '@angular/router-deprecated',
     '@angular/testing',
     '@angular/upgrade',
